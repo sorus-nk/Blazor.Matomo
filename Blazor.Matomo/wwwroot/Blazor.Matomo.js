ba712165e3d639e07273d9202ec9123228d49488
do <d.o.2021@yandex.ru>
2022-10-28 15:12:10 +0700

    window object from module

--- Blazor.Matomo/wwwroot/Blazor.Matomo.js
+++ Blazor.Matomo/wwwroot/Blazor.Matomo.js
@@ -6,16 +6,16 @@ export function init(apiUrl, siteId) {
   console.log("registering", apiUrl, siteId);
   var u = apiUrl;
   u += u.endsWith("/") ? "" : "/"
-  _paq.push(['setTrackerUrl', u + 'matomo.php']);
-  _paq.push(['setSiteId', siteId]);
+  window._paq.push(['setTrackerUrl', u + 'matomo.php']);
+  window._paq.push(['setSiteId', siteId]);
   var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
   g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 }
 export function triggerEvent(url, userId) {
   console.log("Tracking", url);
-  _paq.push(['setCustomUrl', url]);
-  _paq.push(['trackPageView']);
+  window._paq.push(['setCustomUrl', url]);
   if (userId) {
-    _paq.push(['setUserId', userId]);
+    window._paq.push(['setUserId', userId]);
   }
+  window._paq.push(['trackPageView']);
 }
