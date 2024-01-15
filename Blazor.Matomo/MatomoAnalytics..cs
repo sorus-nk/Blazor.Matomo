e0213e88742ba0dd66e54adb3e10ee89a44c1b05
ml-2021 <m.l.2021@yandex.ru>
2024-01-15 18:29:19 +0700

    Update Nuget package version && editorconfig

--- Blazor.Matomo/MatomoAnalytics..cs
+++ Blazor.Matomo/MatomoAnalytics..cs
@@ -7,54 +7,52 @@ using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace Blazor.Matomo
+namespace Blazor.Matomo;
+
+public partial class MatomoAnalytics : ComponentBase, IDisposable
 {
-  public partial class MatomoAnalytics : ComponentBase, IDisposable
-  {
     [Parameter]
     public int SiteId { get; set; }
 
     [Parameter]
-    public string ApiUrl { get; set; } = String.Empty;
+    public string ApiUrl { get; set; } = string.Empty;
 
-    [Inject] NavigationManager NavigationManager { get; set; }
-    [Inject] AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
-    [Inject]
-    protected IJSRuntime? JSRuntime { get; set; }
-    private JsInterop jsInterop;
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+    [Inject] private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+    [Inject] protected IJSRuntime JSRuntime { get; set; } = default!;
+    private JsInterop jsInterop = default!;
     protected override async Task OnInitializedAsync()
     {
-      await base.OnInitializedAsync();
-      NavigationManager.LocationChanged -= OnLocationChanged;
-      NavigationManager.LocationChanged += OnLocationChanged;
-      jsInterop = new(JSRuntime!);
+        await base.OnInitializedAsync();
+        NavigationManager.LocationChanged -= OnLocationChanged;
+        NavigationManager.LocationChanged += OnLocationChanged;
+        jsInterop = new(JSRuntime);
     }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-      await base.OnAfterRenderAsync(firstRender);
-      if (firstRender)
-      {
-        await jsInterop.Init(ApiUrl, SiteId);
-      }
-      await jsInterop.TriggerPageView(NavigationManager.Uri, await GetUserName());
+        await base.OnAfterRenderAsync(firstRender);
+        if (firstRender)
+        {
+            await jsInterop.Init(ApiUrl, SiteId);
+        }
+        await jsInterop.TriggerPageView(NavigationManager.Uri, await GetUserName());
     }
 
     public void Dispose()
     {
-      this.NavigationManager.LocationChanged -= OnLocationChanged;
+        NavigationManager.LocationChanged -= OnLocationChanged;
     }
 
     private async void OnLocationChanged(object? sender, LocationChangedEventArgs args)
     {
-      await jsInterop.TriggerPageView(args.Location, await GetUserName());
+        await jsInterop.TriggerPageView(args.Location, await GetUserName());
     }
 
     private async Task<string?> GetUserName()
     {
-      var b = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-      var uid = b.User.FindFirst(ClaimTypes.NameIdentifier) ?? b.User.FindFirst(ClaimTypes.Name);
-      return uid?.Subject?.Name;
+        AuthenticationState b = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        Claim? uid = b.User.FindFirst(ClaimTypes.NameIdentifier) ?? b.User.FindFirst(ClaimTypes.Name);
+        return uid?.Subject?.Name;
     }
-  }
 }
